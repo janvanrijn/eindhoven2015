a93a32f14d04e4a13aad70fd263c54f2fe50bdcc
janvanrijn <janvanrijn@gmail.com>
2015-06-12 18:47:27 +0200

    typo

--- public_html/header.php
+++ public_html/header.php
@@ -49,7 +49,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">Eindhoven 2014</a>
+          <a class="navbar-brand" href="index.php">Eindhoven 2015</a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
